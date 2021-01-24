eda52dcee73d5096f41245a5e00a99bf874afda4
wanlicc <2499498893@qq.com>
2021-01-24 21:59:57 +0800

    实现持久化登录状态获取

--- src/main/java/life/wan/learn/community/controller/IndexController.java
+++ src/main/java/life/wan/learn/community/controller/IndexController.java
@@ -1,11 +1,39 @@
 package life.wan.learn.community.controller;
 
+import life.wan.learn.community.mapper.UserMapper;
+import life.wan.learn.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/")
-    public String index() { return "index"; }
+    public String index(HttpServletRequest request) throws Exception {
+
+        Cookie[] cookies= request.getCookies();
+
+        if(ObjectUtils.isEmpty(cookies)){
+            throw new Exception("cookies为空");
+        }
+
+        for(Cookie cookie:cookies){
+            if(cookie.getName().equals("token")){
+                String token =cookie.getValue();
+                User user =userMapper.findByToken(token);
+                if(user !=null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+        return "index";
+    }
 }
--- src/main/java/life/wan/learn/community/mapper/UserMapper.java
+++ src/main/java/life/wan/learn/community/mapper/UserMapper.java
@@ -3,12 +3,16 @@ package life.wan.learn.community.mapper;
 import life.wan.learn.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Component;
 
 @Mapper
+@Component
 public interface UserMapper {
 
     @Insert("insert into user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     void insert(User record);
 
-
+    @Select("select * from user where token=#{token} ")
+    User findByToken(String token);
 }
